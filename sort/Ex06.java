cb1a26a9105231ef5b15d61ad358424b091d1d2b
seoyeongchan <115616417+seoyeongchan@users.noreply.github.com>
2022-10-30 20:49:00 +0900

    Update Ex06.java

--- sort/Ex06.java
+++ sort/Ex06.java
@@ -13,13 +13,18 @@ public class Ex06 {
 
 	public static void main(String[] args) {
 		int[] arr = { 2, 7, 8, 4, 6 };
-		showArray(arr);
+		showArray(arr);		// 2 7 8 4 6 
 		
 		Arrays.sort(arr); 	// 별도의 객체를 생성하지 않아도, 정렬 함수를 호출할 수 있다(static)
-		showArray(arr);
-		
+		showArray(arr);		// 2 4 6 7 8 
 		
  	}
+	/*
+	 * 배열을 내림차순으로 정렬할 때는 Collections 클래스의 reverseOrder() 함수를 사용하면 됩니다. 
+	 * 만약 기본 타입 배열을 내림차순으로 정렬하고 싶다면 기본 타입의 배열을 래퍼클래스로 만들어 Comparator를 
+	 * 두번째 인자에 넣어주어야 역순으로 정렬할 수 있습니다.
+	 */
+	
 
 }
 
